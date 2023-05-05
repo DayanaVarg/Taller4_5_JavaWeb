17c37c4249f9e65a854b1b6a8b31a2ee180da21e
DayanaVarg <128272265+DayanaVarg@users.noreply.github.com>
2023-05-04 22:44:58 -0500

    Add files via upload

---
+++ project-2687351/src/main/java/co/edu/sena/project2687351/util/ConnectionPool.java
@@ -0,0 +1,32 @@
+package co.edu.sena.project2687351.util;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionPool {
+    private static String url="jdbc:mysql://localhost:3308/myapp";
+    private static String user="my_app";
+    private static String pass="1234";
+    private static BasicDataSource pool;
+
+    public static BasicDataSource getInstance() throws SQLException{
+        if (pool==null){
+            pool= new BasicDataSource();
+            pool.setUrl(url);
+            pool.setUsername(user);
+            pool.setPassword(pass);
+
+            pool.setInitialSize(5);
+            pool.setMinIdle(5);
+            pool.setMaxIdle(10);
+            pool.setMaxTotal(10);
+        }
+        return pool;
+    }
+    public static Connection getConection()
+        throws SQLException{
+        return getInstance().getConnection();
+    }
+}
